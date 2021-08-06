93b7e057fa3335191bd0e8056f9f2355398d3de9
Damien BATTISTELLA <battistella@protonmail.com>
2021-08-06 15:44:24 +0200

    feat: add new data to devices

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const mqtt = require('mqtt')
 const { createServer } = require('http')
 const {
-  postState, writeState, getStates, readStates,
+  postState, writeState, getStates, readStates, updateState,
 } = require('./state')
 
 const PORT = 9002
@@ -43,33 +43,85 @@ mqttClient.on('connect', () => {
     }
   })
 
+  mqttClient.subscribe('tele/#', (err) => {
+    if (err) {
+      console.error(err)
+    }
+  })
+
   // mqttClient.publish('cmnd/tasmota_6C09EE/Power2')
 })
 
 mqttClient.on('message', (topic, message) => {
   // console.log(`${topic}:${message.toString()}`)
 
+  if (topic.startsWith('tele/') && topic.endsWith('/LWT')) {
+    const deviceId = (topic.split('/'))[1]
+    updateState(deviceId, {
+      status: message.toString(),
+    })
+  }
+
+  if (topic.startsWith('tele/') && topic.endsWith('/STATE')) {
+    try {
+      const deviceId = (topic.split('/'))[1]
+      const data = JSON.parse(message.toString())
+      const updateData = {
+        status: 'Online',
+        uptimeSec: data.UptimeSec,
+        lastUpdate: data.Time,
+      }
+      if (data.POWER) { updateData.POWER = data.POWER }
+      if (data.POWER1) { updateData.POWER = data.POWER1 }
+      if (data.POWER2) { updateData.POWER = data.POWER2 }
+      if (data.Wifi) { updateData.wifi = data.Wifi }
+
+      updateState(deviceId, updateData)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  if (topic.startsWith('tele/') && topic.endsWith('/SENSOR')) {
+    try {
+      const deviceId = (topic.split('/'))[1]
+      const data = JSON.parse(message.toString())
+      updateState(deviceId, {
+        status: 'Online',
+        lastUpdate: data.Time,
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   // Discovery and update devices list
   if (topic.startsWith('tasmota/discovery') && topic.endsWith('config')) {
-    const data = JSON.parse(message.toString())
-    writeState(
-      {
+    try {
+      const data = JSON.parse(message.toString())
+      writeState({
         id: data.t,
         ip: data.ip,
         mac: data.mac,
         model: data.md,
-      },
-    )
+      })
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   // Update devices state
   if (topic.startsWith('stat/') && topic.endsWith('RESULT')) {
-    const deviceId = (topic.split('/'))[1]
-    const devices = readStates()
-    const deviceIndex = devices.findIndex((device) => device.id === deviceId)
-    const data = JSON.parse(message.toString())
-    const device = { ...devices[deviceIndex], ...data }
-    writeState(device)
+    try {
+      const deviceId = (topic.split('/'))[1]
+      const devices = readStates()
+      const deviceIndex = devices.findIndex((device) => device.id === deviceId)
+      const data = JSON.parse(message.toString())
+      const device = { ...devices[deviceIndex], ...data }
+      writeState(device)
+    } catch (err) {
+      console.error(err)
+    }
   }
 })
 
--- src/state.js
+++ src/state.js
@@ -10,6 +10,22 @@ function readStates() {
   return JSON.parse(fs.readFileSync(filePath))
 }
 
+// Update device state by id
+function updateState(deviceId, data) {
+  try {
+    const devices = JSON.parse(fs.readFileSync(filePath))
+    const deviceIndex = devices.findIndex((device) => device.id === deviceId)
+    if (deviceIndex > -1) {
+      devices[deviceIndex] = { ...devices[deviceIndex], ...data }
+      fs.writeFileSync(filePath, JSON.stringify(devices))
+    }
+    return true
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+// Update device state by mac address or add a new device
 function writeState(data) {
   try {
     const states = JSON.parse(fs.readFileSync(filePath))
@@ -44,5 +60,5 @@ function postState(req, res) {
 }
 
 module.exports = {
-  getStates, postState, readStates, writeState,
+  getStates, postState, updateState, readStates, writeState,
 }
